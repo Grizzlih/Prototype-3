d319e95a71a85cbafe005e13e8977984f0ba9214
Edward <ed.gizbreht@gmail.com>
2020-04-01 23:56:21 +0300

    Initial commit

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    Vector3 spawnPos = new Vector3(35, 0, 0);
+    float spawnDelay = 2;
+    float repeatRate = 2;
+    PlayerController playerControllerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstable", spawnDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstable()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+    }
+}
